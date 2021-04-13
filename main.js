14804b59d0dac24ca3db0b7e1cca2908ecb64b84
jana meixnerova <meixnerovajana@gmail.com>
2021-04-13 20:28:08 +0200

    final

--- main.js
+++ main.js
@@ -1,31 +1,52 @@
-// Použij výpis do konzole prohlížeče pomocí console.log('text')
+/* // Použij výpis do konzole prohlížeče pomocí console.log('text')
 // Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
 
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
-console.log('První příklad - vzestupná řada 0 - 10');
 
-//TADY NAPIS CYKLUS
 
-console.log('-------------------');
-
+for (let i = 0; i <11; i++) {
+   console.log(i);
+}
 
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
 
+
+
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
-//TADY NAPIS CYKLUS
+for (let i = 0; i <=10; i++) {
+    
+console.log(i);
+}
 
-console.log('-------------------');
+console.log('------------------');
 
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
-//TADY NAPIS CYKLUS
+
+for (let i = 10; i >=0; i--) {
+   
+console.log(i);
+}
+
+let i =10;
+while(i>0)
+{console.log(i);
+    i--;
+
+}
+ */
+for (let i=0;i<10;i++){
+for (let j =0; j<5;j++){
+    console.log (i + "  " +j)
+}
+}
 
 console.log('-------------------');
