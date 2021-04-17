c8cc0638953d991814c8189ef0f10f0eec4a11df
jana meixnerova <meixnerovajana@gmail.com>
2021-04-17 20:35:07 +0200

    first commit

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-/* // Použij výpis do konzole prohlížeče pomocí console.log('text')
+// Použij výpis do konzole prohlížeče pomocí console.log('text')
 // Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
 
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
 
 
-for (let i = 0; i <11; i++) {
-   console.log(i);
-}
+
+/*  for (let i = 0; i <11; i++) {
+   console.log(i); */
+
+   let i =0;
+   while (i<11){
+       console.log(i);
+       i++;
+   }
+
+
 
 
 // Druhý příklad - zkopíruj předchozí příklad
@@ -18,10 +26,18 @@ for (let i = 0; i <11; i++) {
 
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
-for (let i = 0; i <=10; i++) {
+// for (let i = 0; i <=10; i++) {
     
-console.log(i);
+// console.log(i);
+// }
+
+let j= 0;
+while (j<=10){
+    console.log(j);
+    j++;
 }
+
+
 
 console.log('------------------');
 
@@ -31,22 +47,18 @@ console.log('------------------');
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
 
-for (let i = 10; i >=0; i--) {
+// for (let i = 10; i >=0; i--) {
    
-console.log(i);
+// console.log(i);
+// }
+
+let k =10;
+while(k>0)
+{console.log(k);
+    k--;
+
 }
 
-let i =10;
-while(i>0)
-{console.log(i);
-    i--;
 
-}
- */
-for (let i=0;i<10;i++){
-for (let j =0; j<5;j++){
-    console.log (i + "  " +j)
-}
-}
 
 console.log('-------------------');
